79ffe046f505f2fceb69e3c4b20e89377189727e
Lasse R <lasserafn@gmail.com>
2023-06-25 22:43:58 +0800

    Update uploader.sh

--- uploader.sh
+++ uploader.sh
@@ -280,7 +280,7 @@ fi
 ########## GIT DIFF ##########
 if [ "${ci_branch}" != "" ] && [ "${ci_base_branch}" != "" ]; then
     echo "${ci_branch} ${ci_base_branch}"
-    base_commit_sha=$(git rev-list $(git rev-list --first-parent ^${ci_branch} ${ci_base_branch} | tail -n1)^^!)
+    base_commit_sha=$(git rev-list $(git rev-list --first-parent ^"'${ci_branch}'" "'${ci_base_branch}'" | tail -n1)^^!)
 else
     base_commit_sha=${commit_parent}
 fi
